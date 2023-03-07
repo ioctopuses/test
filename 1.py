f8222b18ccbaba29ff92339ec992c21ebb8e0445
zhangyu <octopuses@foxmail.com>
2023-03-07 16:19:42 +0800

    11111

--- 1.py
+++ 1.py
@@ -1,1 +1,12 @@
-111
+# -*- coding: UTF-8 -*-
+'''
+Author       : zhangyu
+Date         : 2023-03-07 16:17:11
+LastEditors  : zhangyu
+LastEditTime : 2023-03-07 16:19:20
+FilePath     : /test/1.py
+Description  : 
+
+Copyright (c) 2023 by zhangyu, All Rights Reserved.
+'''
+print("zhangyu")
